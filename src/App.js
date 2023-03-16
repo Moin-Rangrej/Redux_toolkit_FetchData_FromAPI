86f4a403b405fe2c969545b34df997139743e33a
Moin Rangrej <rangrejmoin1234@gmail.com>
2023-03-16 10:37:04 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Cakeview from "./components/Cakeview";
 import Countview from "./components/Countview";
+import Navbar from "./components/Navbar";
 import Showuserdata from "./components/Showuserdata";
 import Todoview from "./components/Todoview";
 
 function App() {
   return (
-    <div className="App">
+    <div>
+    <Navbar />
       <Countview />
       <Showuserdata />
       <Todoview />
---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+
+const Navbar = () => {
+    const count = useSelector(state => state.countnum.count)
+    const cakecount = useSelector(state => state.cake.numOfcake)
+    console.log("from navbar" , count + cakecount);
+    console.log("from navbar" , cakecount);
+  return (
+    <div style={{backgroundColor : 'lightblue',height:"100px",width:"auto",margin:'20px'}}>
+      <div style={{backgroundColor: "green",height:'25px',width:'100px',textAlign:'center',borderRadius:'10px',float:'right',margin:'10px'}}>Cart
+        <button style={{backgroundColor:"Highlight",color:'white'}}>{count + cakecount}</button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
